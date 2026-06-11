6ff31c0b078b4b3b9f7e1879cdbc047def7ecda7
JeanTeixeira30 <teixeira.jean30@escola.pr.gov.br>
2026-06-11 08:05:14 -0300

    agrooo

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Mensagem de boas-vindas
-console.log("Bem-vindo ao Agro Forte - Futuro Sustentável!");
-
-// Exemplo de interatividade simples
-document.addEventListener('DOMContentLoaded', () => {
-    alert("Explore o equilíbrio entre produção e meio ambiente!");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site Agro Forte carregado!");
 });
